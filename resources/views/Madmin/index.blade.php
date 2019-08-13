0fa89e820f73a009a4ea0bb33956df2259a12c71
Tivkaa Luper <tivkaaluper@gmail.com>
2019-08-13 23:20:06 +0100

    Create charts

--- resources/views/Madmin/index.blade.php
+++ resources/views/Madmin/index.blade.php
@@ -8,10 +8,8 @@
         <div class="card-panel pink-text center">
             <i class="fas fa-user-friends"></i>
             <div>
-                <h6>Charts</h6>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo ipsam vitae quis unde exercitationem
-                    iste facilis qui nesciunt enim deleniti doloribus labore corrupti, quasi aspernatur sint. Quis nemo
-                    veniam iusto?</p>
+                <h6>Membership Spread</h6>
+                {!! $chart->container() !!}
             </div>
         </div>
     </div>
@@ -20,15 +18,13 @@
         <div class="card-panel  pink-text center">
             <i class="fas fa-plus-circle"></i>
             <div>
-                <h6>Charts</h6>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo ipsam vitae quis unde exercitationem
-                    iste facilis qui nesciunt enim deleniti doloribus labore corrupti, quasi aspernatur sint. Quis nemo
-                    veniam iusto?</p>
+                <h6>Loan Status</h6>
+                {!! $item->container() !!}
             </div>
         </div>
     </div>
 
-    <div class="col s12 m6 l6">
+    {{-- <div class="col s12 m6 l6">
         <div class="card-panel pink-text center">
             <i class="fas fa-user-friends"></i>
             <div>
@@ -38,9 +34,9 @@
                     veniam iusto?</p>
             </div>
         </div>
-    </div>
+    </div> --}}
 
-    <div class="col s12 m6 l6">
+    {{-- <div class="col s12 m6 l6">
         <div class="card-panel  pink-text center">
             <i class="fas fa-plus-circle"></i>
             <div>
@@ -50,6 +46,6 @@
                     veniam iusto?</p>
             </div>
         </div>
-    </div>
+    </div> --}}
 </div>
 @endsection
