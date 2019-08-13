7cf334f18874f38fb9fd43344e15951110ebc4eb
Tivkaa Luper <tivkaaluper@gmail.com>
2019-08-13 23:20:50 +0100

    Refactor content

--- resources/views/inc/dashboard-userreviews.blade.php
+++ resources/views/inc/dashboard-userreviews.blade.php
@@ -1,24 +1,8 @@
 <figure class="review">
-    <h5 class="review__heading">[ <a href='/Dashboard/myPendingLoans/{{auth()->id()}}'>{{$myPendingApp->count()}}</a> ]
-        Pending
-        Application(s)</h5>
-    <blockquote class="review__text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, architecto.
-    </blockquote>
-    <figcaption class="review__user">
-        <img src="{{asset('images/ternenge.jpg')}}" alt="review photo" class="review__photo">
-        <div class="review__user-box">
-            <p class="review__user-name">Ternenge Torough</p>
-            <p class="review__user-date">Feb 23rd, 2019</p>
-        </div>
-        <div class="review__rating">
-            7.9
-        </div>
-    </figcaption>
-</figure>
-
-<figure class="review">
     <h5 class="review__heading">All Paid <a href='/Dashboard/myPaidLoans/{{auth()->id()}}'>{{$paid->count()}}</a></h5>
+    <h5 class="review__heading">Pending <a
+            href='/Dashboard/myPendingLoans/{{auth()->id()}}'>{{$myPendingApp->count()}}</a>
+    </h5>
     <blockquote class="review__text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, architecto.
     </blockquote>
