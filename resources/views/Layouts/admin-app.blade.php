ec640c4374c161dc264b671660da501c4b29ea6e
Tivkaa Luper <tivkaaluper@gmail.com>
2019-08-13 23:19:24 +0100

    Add chart asset

--- resources/views/Layouts/admin-app.blade.php
+++ resources/views/Layouts/admin-app.blade.php
@@ -164,6 +164,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js "></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="{{asset( 'js/echarts.min.js')}} "></script>
     <script>
         //HIDE ALL SECTIONS
     //$('.section').hide();
@@ -270,7 +271,8 @@ $('.deny').click(function(e){
      })
     </script>
     <script src="{{asset( 'js/custom.js')}} "></script>
-
+    {!! $chart->script() !!}
+    {!! $item->script() !!}
 </body>
 @toastr_render
 
