10e6cd528b5b684043337e5dac05ccf7c1d97165
Tivkaa Luper <tivkaaluper@gmail.com>
2019-08-13 23:19:04 +0100

    Add chart

--- resources/views/Dashboard/home.blade.php
+++ resources/views/Dashboard/home.blade.php
@@ -2,9 +2,10 @@
 @section('admin')
 
 
-<p class="paragraph">Recent Deductions
-</p>
-
+<p class="paragraph">MY ACTIVITY</p>
+<div>
+    {!! $footPrints->container() !!}
+</div>
 {{-- <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, velit nemo! Aperiam cum
     reprehenderit
     unde velit dolorum minima? Cum repellat enim assumenda eaque quasi eveniet nulla mollitia accusamus
