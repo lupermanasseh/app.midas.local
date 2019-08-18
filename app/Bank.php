066fe974e336c6c86fbc0dbf0808bc8028e3ead2
Tivkaa Luper <tivkaaluper@gmail.com>
2019-08-18 17:39:25 +0100

    Added fillable properties

--- app/Bank.php
+++ app/Bank.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
+
+    protected $fillable = [
+        'bank_name',
+        'acct_number',
+        'user_id',
+    ];
     //Defining relationship with user bank details
     public function user(){
         return $this->belongsTo(User::class);
