ff791e42034a878b969041e2007e3c327cf43d3c
Tivkaa Luper <tivkaaluper@gmail.com>
2019-08-20 06:31:41 +0100

    log files

---
+++ storage/framework/views/6dd2e1b32fb8aee18d3de24ca24b0ee82c1880bd.php
@@ -0,0 +1,21 @@
+<table class="highlight">
+    <thead>
+        <tr>
+            <th>USER ID</th>
+            <th>NAME</th>
+            <th>DATE</th>
+            <th>AMOUNT</th>
+            <th>DESCRIPTION</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $__currentLoopData = $savings; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $active): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+            <td><?php echo e($active->user_id); ?></td>
+            <td><?php echo e($active->user->first_name); ?> <?php echo e($active->user->last_name); ?></td>
+            <td><?php echo e(now()->toDateString()); ?></td>
+            <td><?php echo e($active->current_amount); ?></td>
+        </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </tbody>
+</table>
