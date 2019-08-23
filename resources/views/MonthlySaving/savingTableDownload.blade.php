0e7d17528474657a19272479fbac26f9e2e7150f
Tivkaa Luper <tivkaaluper@gmail.com>
2019-08-23 11:52:49 +0100

    Added default date

--- resources/views/MonthlySaving/savingTableDownload.blade.php
+++ resources/views/MonthlySaving/savingTableDownload.blade.php
@@ -3,8 +3,8 @@
         <tr>
             <th>USER ID</th>
             <th>NAME</th>
-            <th>DATE</th>
             <th>AMOUNT</th>
+            <th>DATE</th>
             <th>DESCRIPTION</th>
         </tr>
     </thead>
@@ -13,8 +13,8 @@
         <tr>
             <td>{{$active->user_id}}</td>
             <td>{{$active->user->first_name}} {{$active->user->last_name}}</td>
-            {{-- <td>{{now()->toDateString()}}</td> --}}
             <td>{{$active->current_amount}}</td>
+            <td>{{now()->toDateString()}}</td>
         </tr>
         @endforeach
     </tbody>
