26bafed74b06d04c409c9d3f676df4ca8ac43e72
Tivkaa Luper <tivkaaluper@gmail.com>
2019-09-05 09:34:56 +0100

    logged files

---
+++ storage/framework/views/e0cfb85ec7c99d0f96dad4ef4d00d8b8b4ad80e3.php
@@ -0,0 +1,23 @@
+<?php $__env->startSection('user-signin'); ?>
+<div></div>
+<div class="user-signinlogocontainer">
+    <img src="<?php echo e(asset('images/logo.png')); ?>" alt="" class="logo-usersignin">
+</div>
+<div class="user-signinform">
+    <form class="user-customsearch" method="POST" action="/Dashboard/user/customsearch">
+        <?php echo e(csrf_field()); ?>
+
+
+        <div class="user-customsearch__item"><input class="custom-input" type="text" name="username" id="username"
+                placeholder="Username"></div>
+
+        <div class="user-customsearch__item"><input class="custom-input" type="password" name="password" id="password"
+                placeholder="Password"></div>
+
+        <div class="user-customsearch__item">
+            <button class="user-custom-input" type="submit">Login</button>
+        </div>
+    </form>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('Layouts.userSigninLayout', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
