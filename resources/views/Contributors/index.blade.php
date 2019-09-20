dbb5d16d525dcccf6d1e59db8b0ac679c1713015
Tivkaa Luper <tivkaaluper@gmail.com>
2019-09-20 03:31:29 +0100

    Removed headings

--- resources/views/Contributors/index.blade.php
+++ resources/views/Contributors/index.blade.php
@@ -1,17 +1,11 @@
-@extends('Layouts.admin-app') 
+@extends('Layouts.admin-app')
 @section('main-content')
 <div class="container">
     {{--
     @include('inc.messages') --}}
     <div class="row">
         <div class="col s12 subject-header">
-            <p class="teal-text">Active Monthly Deductions</p>
-            <span><a href="/"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="New Loan Subscription">playlist_add</i></a></span>
-            <span><a href="/saving/search"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Search Savings">search</i></a></span>
-            <span><a href="/products"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Upload Savings">cloud_upload</i></a></span>
-            <span><a href="/"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="All User Savings">view_list</i></a></span>
-            <span><a href="/products"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="All Savings">visibility</i></a></span>
-            <span><a href="{{route('usersaving.create')}}"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="New Savings Upload">cloud_upload</i></a></span>
+            <p class="teal-text">Active Members</p> 
         </div>
     </div>
 
@@ -21,7 +15,7 @@
             <table class="highlight">
                 <thead>
                     <tr>
-                        <th>Contributor Name</th>
+                        <th>Name</th>
                         <th>Status</th>
                         <th>Saving Items</th>
                     </tr>
