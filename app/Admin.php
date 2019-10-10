a06435a24d2d80aa31ce20d5fc30b025de8b2bae
Tivkaa Luper <tivkaaluper@gmail.com>
2019-10-10 14:00:18 +0100

    Added admin model

---
+++ app/Admin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'other_name',
+        'sex',
+        'phone',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
