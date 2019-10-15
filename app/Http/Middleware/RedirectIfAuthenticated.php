03a9d79b8fd253b44105ae6686bc796260e2f86d
Tivkaa Luper <tivkaaluper@gmail.com>
2019-10-15 11:02:40 +0100

    Handle authenticated redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,8 +17,12 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        if ($guard == "admin" && Auth::guard($guard)->check()) {
+            return redirect('/admin');
+        }
+        
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect('/Dashboard/home');
         }
 
         return $next($request);
