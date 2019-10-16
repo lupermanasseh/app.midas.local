d2ddd6f80ec2d049051ec2ae372823556169c2d4
Tivkaa Luper <tivkaaluper@gmail.com>
2019-10-16 13:31:23 +0100

    log files

---
+++ storage/framework/views/8f81b5b90a54efc8d49bd42a87bf5b7209da0467.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?famaily=Open+Sans:300,400,600">
+    <link rel="stylesheet" href="<?php echo e(asset('css/portal.css')); ?>">
+    <title><?php echo e($title); ?></title>
+</head>
+
+<body>
+    <div class="midas-container">
+        
+        <?php echo $__env->make('inc.usersignin-header', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+        
+        <div class="midas-content">
+
+            <main class="midas-view">
+
+
+                <div class="detail">
+                    
+                    <div class="description">
+                        
+                        <?php echo $__env->yieldContent('user-signin'); ?>
+
+                    </div>
+
+
+                </div>
+
+            </main>
+
+        </div>
+    </div>
+
+    
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+
+</html>
