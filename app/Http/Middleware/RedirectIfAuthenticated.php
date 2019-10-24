d5dd2cac79790a887db06e878282f4ace51bc29c
Tivkaa Luper <tivkaaluper@gmail.com>
2019-10-24 12:10:05 +0100

    Added redirect login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,13 +17,37 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if ($guard == "admin" && Auth::guard($guard)->check()) {
-            return redirect('/admin');
-        }
+        // if ($guard == "admin" && Auth::guard($guard)->check()) {
+        //     return redirect('/admin');
+        // }
         
-        if (Auth::guard($guard)->check()) {
-            return redirect('/Dashboard');
-        }
+        // if (Auth::guard($guard)->check()) {
+        //     return redirect('/Dashboard');
+        // }
+
+        switch ($guard) {
+            case 'admin':
+              if (Auth::guard($guard)->check()) {
+               // return redirect()->route('admin.dashboard');
+               return redirect('/admin');
+              }
+              break;
+            //   case 'doctor':
+            //   if (Auth::guard($guard)->check()) {
+            //     return redirect()->route('doctor.dashboard');
+            //   }
+            //   break;
+            //   case 'consultant':
+            //   if (Auth::guard($guard)->check()) {
+            //     return redirect()->route('consultant.dashboard');
+            //   }
+            //   break;
+            default:
+              if (Auth::guard($guard)->check()) {
+                  return redirect('/Dashboard');
+              }
+              break;
+          }
 
         return $next($request);
     }
