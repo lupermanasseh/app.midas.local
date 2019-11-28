239d34a920e1572dabb5ee572016acbc50edf425
Tivkaa Luper <tivkaaluper@gmail.com>
2019-11-28 15:15:11 +0100

    Added created by id field

--- app/Imports/SavingReviewUserImport.php
+++ app/Imports/SavingReviewUserImport.php
@@ -20,6 +20,7 @@ class SavingReviewUserImport implements ToModel,WithHeadingRow
             //
             'current_amount' => $row['contribution'],
             'user_id' => $row['reg_no'],
+            'created_by' => auth()->id(),
         ]);
     }
 }
