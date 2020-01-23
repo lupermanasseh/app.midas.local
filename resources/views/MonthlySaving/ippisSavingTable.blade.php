e6c173cc5740a739a256cba12d8fafaf8b614a07
Tivkaa Luper <tivkaaluper@gmail.com>
2020-01-23 16:11:08 +0100

    Corrected logic error

--- resources/views/MonthlySaving/ippisSavingTable.blade.php
+++ resources/views/MonthlySaving/ippisSavingTable.blade.php
@@ -1,7 +1,7 @@
 <table class="highlight">
     <thead>
         <tr>
-            {{-- <th>S/NO</th> --}}
+            <th>S/NO</th>
             <th>IPPIS NO</th>
             <th>NAME</th>
             <th>SAVING</th>
@@ -12,12 +12,12 @@
     <tbody>
         @foreach ($savings as $active)
         <tr>
-            {{-- <td>{{$loop->iteration}}</td> --}}
+            <td>{{$loop->iteration}}</td>
             <td>{{$active->user->payment_number}}</td>
-            <td>{{$active->user->first_name}} {{$active->user->last_name}}</td>
+            <td>{{$active->user->last_name}} {{$active->user->first_name}}</td>
             <td>{{number_format($active->current_amount,2,'.',',')}}</td>
-            <td>{{number_format($active->tsActiveAmount($active->user_id,$ts),2,'.',',')}}</td>
-            <td>{{number_format($active->ippisSavingSum($active->current_amount,$active->tsActiveAmount      ($active->user_id,$ts)),2,'.',',')}}
+            <td>{{number_format($active->tsActiveAmount($active->user_id),2,'.',',')}}</td>
+            <td>{{number_format($active->current_amount + $active->tsActiveAmount($active->user_id))}}
             </td>
         </tr>
         @endforeach
