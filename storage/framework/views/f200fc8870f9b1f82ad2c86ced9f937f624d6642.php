54db730cab924fecc664e2cf84238cfcb130c897
Tivkaa Luper <tivkaaluper@gmail.com>
2020-01-23 16:11:36 +0100

    log files

--- storage/framework/views/f200fc8870f9b1f82ad2c86ced9f937f624d6642.php
+++ storage/framework/views/f200fc8870f9b1f82ad2c86ced9f937f624d6642.php
@@ -1,7 +1,7 @@
 <table class="highlight">
     <thead>
         <tr>
-            
+            <th>S/NO</th>
             <th>IPPIS NO</th>
             <th>NAME</th>
             <th>SAVING</th>
@@ -12,12 +12,12 @@
     <tbody>
         <?php $__currentLoopData = $savings; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $active): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
         <tr>
-            
+            <td><?php echo e($loop->iteration); ?></td>
             <td><?php echo e($active->user->payment_number); ?></td>
-            <td><?php echo e($active->user->first_name); ?> <?php echo e($active->user->last_name); ?></td>
+            <td><?php echo e($active->user->last_name); ?> <?php echo e($active->user->first_name); ?></td>
             <td><?php echo e(number_format($active->current_amount,2,'.',',')); ?></td>
-            <td><?php echo e(number_format($active->tsActiveAmount($active->user_id,$ts),2,'.',',')); ?></td>
-            <td><?php echo e(number_format($active->ippisSavingSum($active->current_amount,$active->tsActiveAmount      ($active->user_id,$ts)),2,'.',',')); ?>
+            <td><?php echo e(number_format($active->tsActiveAmount($active->user_id),2,'.',',')); ?></td>
+            <td><?php echo e(number_format($active->current_amount + $active->tsActiveAmount($active->user_id))); ?>
 
             </td>
         </tr>
