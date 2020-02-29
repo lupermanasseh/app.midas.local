fcc374d6d5fdd3d98073b6f31986012ec8369583
Tivkaa Luper <tivkaaluper@gmail.com>
2020-02-29 12:52:40 +0100

    commented out code

--- resources/views/Layouts/admin-app.blade.php
+++ resources/views/Layouts/admin-app.blade.php
@@ -166,7 +166,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js "></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="{{asset( 'js/echarts.min.js')}} "></script>
-    <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+    {{-- <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script> --}}
     @stack('scripts')
     <script>
         //HIDE ALL SECTIONS
