d31bb9243199a187de111d55f403ebfacac0658f
Tivkaa Luper <tivkaaluper@gmail.com>
2020-02-29 06:50:46 +0100

    Refacored members page to use datatable

--- resources/views/Contributors/index.blade.php
+++ resources/views/Contributors/index.blade.php
@@ -5,36 +5,58 @@
     @include('inc.messages') --}}
     <div class="row">
         <div class="col s12 subject-header">
-            <p class="teal-text">Active Members</p>
+            <p class="teal-text">Membership Register</p>
         </div>
     </div>
 
     <div class="row">
         <div class="col s12">
-            @if (count($activeUsers)>=1)
-            <table class="highlight">
+            <table class="highlight" id="users-table">
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Status</th>
-                        <th>Saving Items</th>
+                        <th>REG ID</th>
+                        <th>LAST NAME</th>
+                        <th>FIRST NAME</th>
+                        <th>MEMBERSHIP TYPE</th>
+                        <th>IPPIS</th>
+                        <th>STATUS</th>
                     </tr>
                 </thead>
-                <tbody>
+                {{-- <tbody>
                     @foreach ($activeUsers as $user)
                     <tr>
-                        <td><a href="/saving/listings/{{$user->id}}">{{$user->first_name}} {{$user->last_name}}</a></td>
-                        <td>{{$user->status}}</td>
-                        <td>{{$user->usersavings_count}}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
+                        <td><a href="/saving/listings/{{$user->id}}">{{$user->first_name}} {{$user->last_name}}</a>
+                </td>
+                <td>{{$user->status}}</td>
+                <td>{{$user->usersavings_count}}</td>
+                </tr>
+                @endforeach
+                </tbody> --}}
             </table>
             {{-- {{$activeUsers->links()}} --}}
-            @else
+            {{-- @else
             <p>No Records Yet</p>
-            @endif
+            @endif --}}
         </div>
     </div>
 </div>
 @endsection
+@push('scripts')
+<script>
+    $(function() {
+    $('#users-table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: '{!! route('users.data') !!}',
+        columns: [
+            { data: 'id', name: 'id' },
+            { data: 'last_name', name: 'last_name' },
+            { data: 'first_name', name: 'first_name' },
+            { data: 'membership_type', name: 'membership_type' },
+            { data: 'payment_number', name: 'payment_number' },
+            { data: 'status', name: 'status' }
+        ]
+    });
+});
+</script>
+@endpush
