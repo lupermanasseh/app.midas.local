f494ccff5a1f279dc190e2f85366b5b84ad9a364
Tivkaa Luper <tivkaaluper@gmail.com>
2020-04-17 12:54:46 +0100

    New legacy loan deduction import

---
+++ app/Imports/legacyLoanDeductionImport.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Imports;
+
+use App\Ldeduction;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class legacyLoanDeductionImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+
+    public function __construct($rand){
+     
+        $this->string = $rand;
+       
+    }
+
+    public function model(array $row)
+    {
+        return new Ldeduction([
+            //
+            'product_id' => $row['product_id'],
+            'user_id' => $row['reg_no'],
+            'lsubscription_id' => $row['sub_id'],
+            'amount_deducted' => $row['amount'],
+            'entry_month' => \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['entry_date']),
+            'notes' => $row['description'],
+            'deduct_reference' => $this->string,
+        ]);
+    }
+}
