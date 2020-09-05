8f965a0881da9f7e7b5e59533839bfdbf10ff524
Tivkaa Luper <tivkaaluper@gmail.com>
2020-09-05 16:59:32 +0100

    Removed start date field

--- resources/views/LoanDeduction/loanBalancesFind.blade.php
+++ resources/views/LoanDeduction/loanBalancesFind.blade.php
@@ -12,17 +12,8 @@
             {{ csrf_field() }}
             <div class="row">
 
-                 <!-- <div class="input-field col s12 m4 l4">
-                    <input id="reg_number" name="reg_number" type="text" placeholder="e.g 78" class="validate" required>
-                    <label for="reg_number">Registration Number</label>
-                </div>  -->
-                <div class="input-field col s12 m6 l6">
-                    <input id="from" name="from" type="date" class="validate" required>
-                    <label for="from">Start Date</label>
-                </div>
-
-                <div class="input-field col s12 m6 l6">
-                    <input id="to" name="to" type="date" class="validate" required>
+                <div class="input-field col s12 m12 l12">
+                    <input id="to" name="to" type="text" class="validate datepicker" required>
                     <label for="to">End Date</label>
                 </div>
 
