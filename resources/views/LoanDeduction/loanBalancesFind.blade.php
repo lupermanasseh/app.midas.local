4dc65e136dabf57b7c6b7e7db3097e7bdc136ee4
Tivkaa Luper <tivkaaluper@gmail.com>
2020-09-08 08:58:33 +0100

    minor edit

--- resources/views/LoanDeduction/loanBalancesFind.blade.php
+++ resources/views/LoanDeduction/loanBalancesFind.blade.php
@@ -11,12 +11,10 @@
         <form class="col s12" method="POST" action="/loanbalances/find">
             {{ csrf_field() }}
             <div class="row">
-
                 <div class="input-field col s12 m12 l12">
                     <input id="to" name="to" type="text" class="validate datepicker" required>
                     <label for="to">End Date</label>
                 </div>
-
             </div>
 
             <button type="submit" class="btn">Find</button>
