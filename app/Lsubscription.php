a16e08b0be59154e53cc79a78c520db074d1dceb
Tivkaa Luper <tivkaaluper@gmail.com>
2020-09-12 12:29:53 +0100

    Edited function to cater for top up loans

--- app/Lsubscription.php
+++ app/Lsubscription.php
@@ -151,6 +151,8 @@ public function allLoanBalancesByDate($collection,$id)
     foreach($user_subscriptions as $item){
         //$totalBal=0;
         $approved_amt = $item->amount_approved;
+        $topup_amt = $item->topup_amount;
+        $totalLoanAmount = $approved_amt + $topup_amt;
         //select all deductions by sub_id
         $deductionCollection = Ldeduction::where('lsubscription_id',$item->id)
                                           ->get();
@@ -158,7 +160,7 @@ public function allLoanBalancesByDate($collection,$id)
         $loanDebit = $deductionCollection->sum('amount_debited');
         $totalDeductions = $loanCredit-$loanDebit;
         //$deductions = $lsub->totalLoanDeductions($item->id);
-        $bal = $approved_amt-$totalDeductions;
+        $bal = $totalLoanAmount-$totalDeductions;
         $sumBal = $sumBal+$bal;
     }
     return $sumBal;
@@ -170,7 +172,7 @@ public function allLoanBalancesByDate($collection,$id)
     public function loanBalanceAggregateAt($collection){
       $sumBal=0;
 
-    //  $unique_subscriptions = $collection->unique('user_id');
+
 
       foreach($collection as $item){
 
