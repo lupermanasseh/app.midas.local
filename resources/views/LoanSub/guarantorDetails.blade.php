41b07783d935fde869686502ef4ca53f8e4e2373
Tivkaa Luper <tivkaaluper@gmail.com>
2020-09-23 16:44:34 +0100

    Refactored display code

--- resources/views/LoanSub/guarantorDetails.blade.php
+++ resources/views/LoanSub/guarantorDetails.blade.php
@@ -50,77 +50,59 @@
 
 
     <div class="row user-profiles">
-      @if($firstGuarantor->count()>=1)
         <div class="col s12 m9 l9 profile-detail">
 
-                <p class="profile__heading text-grey darken-3">First Loan Guaranteed</p>
+                <p class="profile__heading text-grey darken-3">Loans Guaranteed</p>
 
 
 
                 <table class="highlight">
                     <thead>
                         <tr>
-                            <th>LOANEE</th>
-                            <th>PRODUCT</th>
+                            <th>REG</th>
+                            <th>MEMBER</th>
+                            <TH>PRODUCT</TH>
+                            <TH>LOAN BAL</TH>
+                            <TH>LIABILITY</TH>
                             <th>STATUS</th>
                         </tr>
                     </thead>
                     <tbody>
                       @foreach($firstGuarantor as $firstg)
                         <tr>
+                            <td>{{substr($firstg->user->membership_type,0,1)}}/{{$firstg->user_id}}</td>
                             <td>{{$firstg->user->first_name}} {{$firstg->user->last_name}}</td>
                             <td>{{$firstg->product->name}}</td>
+                            <td>{{number_format($firstg->user->singleLoanBalance($firstg->id),2,'.',',')}}</td>
+                            <td>{{number_format($firstg->user->singleLoanBalance($firstg->id)/2,2,'.',',')}}</td>
                             <td>{{$firstg->loan_status}}
                             </td>
-
                         </tr>
                         @endforeach
+                        @foreach($secondGuarantor as $secondg)
+                          <tr>
+                              <td>{{substr($secondg->user->membership_type,0,1)}}/{{$secondg->user_id}}</td>
+                              <td>{{$secondg->user->first_name}} {{$secondg->user->last_name}}</td>
+                              <td>{{$secondg->product->name}}</td>
+                              <td>{{number_format($secondg->user->singleLoanBalance($secondg->id),2,'.',',')}}</td>
+                              <td>{{number_format($secondg->user->singleLoanBalance($secondg->id)/2,2,'.',',')}}</td>
+                              <td>{{$secondg->loan_status}}
+                              </td>
+                          </tr>
+                          @endforeach
+                          @if(count($firstGuarantor)>=1 OR count($secondGuarantor)>=1 )
+                          <tr>
+                              <th colspan="3">Summary</th>
+
+                              <th>{{number_format($newSubObj->totalLiability($newUser->id)*2,2,'.',',')}}</th>
+                              <th>{{number_format($newSubObj->totalLiability($newUser->id),2,'.',',')}}</th>
+                          </tr>
+                          @else
+                          @endif
                     </tbody>
                 </table>
 
         </div>
-        @else
-        <span>You have not guaranteed any loan as a first guarantor</sapn>
-        @endif
-
     </div>
-
-    <div class="row user-profiles">
-      @if($secondGuarantor->count()>=1)
-
-        <div class="col s12 m9 l9 profile-detail">
-
-                <p class="profile__heading text-grey darken-3">Second Loan Guaranteed</p>
-
-
-
-                <table class="highlight">
-                    <thead>
-                        <tr>
-                            <th>LOANEE</th>
-                            <th>PRODUCT</th>
-                            <th>STATUS</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                      @foreach($secondGuarantor as $secondg)
-                        <tr>
-                            <td>{{$secondg->user->first_name}} {{$secondg->user->last_name}}</td>
-                            <td>{{$secondg->product->name}}</td>
-                            <td>{{$secondg->loan_status}}
-                            </td>
-
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-
-        </div>
-        @else
-        <span>You have not guaranteed any loan as a second guarantor</sapn>
-        @endif
-
-    </div>
-
 </div>
 @endsection
--- resources/views/LoanSub/guarantors.blade.php
+++ resources/views/LoanSub/guarantors.blade.php
@@ -8,6 +8,7 @@
             <span class="teal-text">LOAN GUARANTORS</span>
         </div>
     </div>
+
     <!-- <div class="row">
         <div class="col s12 subject-header">
             <span><a href="/pendingLoans"><i class="small material-icons tooltipped" data-position="bottom"
@@ -18,15 +19,19 @@
     <div class="row user-profiles">
         @foreach($uniqueGuarantors as $guarantor)
         <div class="col s12 m3 l3 profile">
+          <p class="profile__heading text-grey darken-3">REG: {{$guarantor}}</p>
             <p class="profile__heading text-grey darken-3">{{$review->user->userInstance($guarantor)->first_name}} {{$review->user->userInstance($guarantor)->last_name}}</p>
             <!-- <span><i class="small material-icons pink-text lighten-4">looks</i></span> -->
             <span>
+              {{number_format($review->totalLiability($guarantor),2,'.',',')}}
+            </span>
+            <!-- <span>
               @if($review->imageCount($guarantor)===0)
               <img src="/images/logo.png" class="guarantor_photo"/>
                   @else
                   <img src="{{$review->user->userInstance($guarantor)->photo}}" alt="" class="guarantor_photo">
                   @endif
-            </span>
+            </span> -->
             <span class="profile__user-name">
               Total Loans Guaranteed
             </span>
