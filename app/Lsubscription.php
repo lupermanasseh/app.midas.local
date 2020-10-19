1b5b27e8e06df13dade70c46a9e7c04164ebe539
Tivkaa Luper <tivkaaluper@gmail.com>
2020-10-19 16:25:45 +0100

    added topup amount

--- app/Lsubscription.php
+++ app/Lsubscription.php
@@ -388,7 +388,7 @@ public function uniqueDebtors($userid){
 public function findCompleteBalance($subid){
     $ldeductionObj = new Ldeduction;
     $loan = Lsubscription::find($subid);
-    $principal = $loan->amount_approved;
+    $principal = $loan->amount_approved+$loan->topup_amount;
 
     $totalDeductions = $ldeductionObj->totalLoanCredit($subid)-$ldeductionObj->totalLoanDebit($subid);
     return $balance = $principal-$totalDeductions;
