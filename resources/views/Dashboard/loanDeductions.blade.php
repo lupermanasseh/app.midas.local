d5f2cc4d712e90d700d43bb4854f470de69793d1
Tivkaa Luper <tivkaaluper@gmail.com>
2020-10-22 17:40:10 +0100

    Refactored loan detail display

--- resources/views/Dashboard/loanDeductions.blade.php
+++ resources/views/Dashboard/loanDeductions.blade.php
@@ -6,23 +6,32 @@
     <thead>
         <tr>
             <th>DATE</th>
-            <th>PRODUCT</th>
-            <th>AMOUNT</th>
             <th>DESC</th>
-
+            <th>DEBIT</th>
+            <th>CREDIT</th>
+            <th>BAL</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($deductions as $list)
         <tr>
             <td>{{$list->entry_month->toFormattedDateString()}}</td>
+            <td>{{$list->notes}}</td>
             <td>
-                {{$list->product->name}}
+              @if($list->amount_debited)
+              {{number_format($list->amount_debited,2,'.',',')}}
+              @else
+              @endif
             </td>
-            <td>{{number_format($list->amount_deducted,2,'.',',')}}</td>
             <td>
-                {{$list->notes}}
-            </td>
+              @if($list->amount_deducted)
+              {{number_format($list->amount_deducted,2,'.',',')}}
+              @else
+              @endif
+              </td>
+              <td>
+              {{number_format($loan->amount_approved-$myItem->balances,2,'.',',')}}
+              </td>
 
         </tr>
         @endforeach
