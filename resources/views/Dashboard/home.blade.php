2dbe9eb579dbec5dfddabddf9ecd9d368c15abcf
Tivkaa Luper <tivkaaluper@gmail.com>
2020-10-22 15:45:08 +0100

    Improved on the display and refactored some methods

--- resources/views/Dashboard/home.blade.php
+++ resources/views/Dashboard/home.blade.php
@@ -8,8 +8,8 @@
     <table class="highlight">
         <thead>
             <tr>
-                <th>#</th>
-                <th>Name</th>
+                <!-- <th>#</th>
+                <th>Name</th> -->
                 <th>Description</th>
                 <th>Status</th>
                 <th>Balance</th>
@@ -18,9 +18,9 @@
         <tbody>
 
             <tr>
-                <td>{{substr($user->membership_type,0,1)}}/{{$user->id}}</td>
+                <!-- <td>{{substr($user->membership_type,0,1)}}/{{$user->id}}</td>
                 <td>
-                    {{$user->first_name}} {{$user->last_name}}</td>
+                    {{$user->first_name}} {{$user->last_name}}</td> -->
                 <td>Savings (Contribution)</td>
                 <td>{{$user->status}}</td>
                 <td>
@@ -47,13 +47,14 @@
     </table>
 </div>
 <div>
-    <p>LOANS</p>
+
     @if(count($activeLoans)>=1)
+    <p>LOANS</p>
     <table class="">
         <thead>
             <tr>
-                <th>#</th>
-                <th>Name</th>
+                <!-- <th>#</th>
+                <th>Name</th> -->
                 <th>Loan Type</th>
                 <th>S/Date</th>
                 <th>E/Date</th>
@@ -69,18 +70,18 @@
 
             @foreach ($activeLoans as $myProduct)
             <tr>
-                <td>{{substr($user->membership_type,0,1)}}/{{$user->id}}</td>
+                <!-- <td>{{substr($user->membership_type,0,1)}}/{{$user->id}}</td>
                 <td>
                     {{$user->first_name}} {{$user->last_name}}
-                </td>
+                </td> -->
                 <td>{{$myProduct->product->name}}</td>
                 <td>{{$myProduct->loan_start_date->toDateString()}}</td>
                 <td>{{$myProduct->loan_end_date->toDateString()}}</td>
                 <td>{{$myProduct->custom_tenor}}</td>
-                <td>{{number_format($myProduct->amount_approved,2,'.',',')}}</td>
+                <td>{{number_format($myProduct->amount_approved+$myProduct->topup_amount,2,'.',',')}}</td>
                 <td>{{number_format($myProduct->monthly_deduction,2,'.',',')}}</td>
                 <td><a
-                        href="/#/{{$myProduct->id}}">{{number_format($myProduct->amount_approved-$myProduct->totalLoanDeductions($myProduct->id),2,'.',',')}}</a>
+                        href="/#/{{$myProduct->id}}">{{number_format($myProduct->amount_approved+$myProduct->topup_amount-$myProduct->totalLoanDeductions($myProduct->id),2,'.',',')}}</a>
                 </td>
             </tr>
             @endforeach
@@ -89,6 +90,60 @@
             @endif
 
             @if(count($activeLoans)>=1)
+            <tr>
+                <th colspan="6">Summary</th>
+                <th>{{number_format($user->totalApprovedAmount(auth()->id()),2,'.',',')}}</th>
+                <th>{{number_format($user->loanSubscriptionTotal(auth()->id()),2,'.',',')}}</th>
+                <th>{{number_format($user->allLoanBalances(auth()->id()),2,'.',',')}}</th>
+            </tr>
+            @else
+            @endif
+
+        </tbody>
+    </table>
+
+    @if(count($inactiveLoans)>=1)
+    <p>INACTIVE LOANS</p>
+    <table class="">
+        <thead>
+            <tr>
+                <!-- <th>#</th>
+                <th>Name</th> -->
+                <th>Loan Type</th>
+                <th>S/Date</th>
+                <th>E/Date</th>
+                <th>Tenor</th>
+                <th>Amt</th>
+                <th>Repymt</th>
+                <th>Bal</th>
+            </tr>
+        </thead>
+        <tbody>
+
+
+
+            @foreach ($inactiveLoans as $inactive)
+            <tr>
+                <!-- <td>{{substr($user->membership_type,0,1)}}/{{$user->id}}</td>
+                <td>
+                    {{$user->first_name}} {{$user->last_name}}
+                </td> -->
+                <td>{{$inactive->product->name}}</td>
+                <td>{{$inactive->loan_start_date->toDateString()}}</td>
+                <td>{{$inactive->loan_end_date->toDateString()}}</td>
+                <td>{{$inactive->custom_tenor}}</td>
+                <td>{{number_format($inactive->amount_approved+$inactive->topup_amount,2,'.',',')}}</td>
+                <td>{{number_format($inactive->monthly_deduction,2,'.',',')}}</td>
+                <td><a
+                        href="/#/{{$myProduct->id}}">{{number_format($inactive->amount_approved+$inactive->topup_amount-$myProduct->totalLoanDeductions($myProduct->id),2,'.',',')}}</a>
+                </td>
+            </tr>
+            @endforeach
+            @else
+            <p>No Inactive Loans available yet</p>
+            @endif
+
+            @if(count($inactiveLoans)>=1)
             <tr>
                 <th colspan="6">Summary</th>
                 <th>{{number_format($user->totalApprovedAmount(auth()->id()),2,'.',',')}}</th>
