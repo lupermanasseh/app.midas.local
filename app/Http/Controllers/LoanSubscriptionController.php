332b228a3f025c5c3631cd0dc8ca07b55ca24f22
Tivkaa Luper <tivkaaluper@gmail.com>
2020-11-26 19:43:08 +0100

    Commented sms code out

--- app/Http/Controllers/LoanSubscriptionController.php
+++ app/Http/Controllers/LoanSubscriptionController.php
@@ -752,14 +752,14 @@ public function destroyLoanDeductions($id)
                           $loan_sub->save();
 
                             //send message
-                              $client = new Client;
-                              $api = '9IGspBnLAjWENmr9nPogQRN9PuVwAHsSPtGi5szTdBfVmC2leqAe8vsZh6dg';
-                              $to = $phone;
-                              $from= 'MIDAS TOUCH';
-                              $message = 'Your '.$product.' facility of N'. $amt_approved.' has been paid and is now active. Thank you. MIDAS TEAM';
-                             $url = 'https://www.bulksmsnigeria.com/api/v1/sms/create?api_token='.$api.'&from='.$from.'&to='.$to.'&body='.$message.'&dnd=1';
-
-                             $response = $client->request('GET', $url,['verify'=>false]);
+                             //  $client = new Client;
+                             //  $api = '9IGspBnLAjWENmr9nPogQRN9PuVwAHsSPtGi5szTdBfVmC2leqAe8vsZh6dg';
+                             //  $to = $phone;
+                             //  $from= 'MIDAS TOUCH';
+                             //  $message = 'Your '.$product.' facility of N'. $amt_approved.' has been paid and is now active. Thank you. MIDAS TEAM';
+                             // $url = 'https://www.bulksmsnigeria.com/api/v1/sms/create?api_token='.$api.'&from='.$from.'&to='.$to.'&body='.$message.'&dnd=1';
+                             //
+                             // $response = $client->request('GET', $url,['verify'=>false]);
 
                              //post amount to user consolidated loans
                              $newConsolidatedDeduct = new Userconsolidatedloan();
