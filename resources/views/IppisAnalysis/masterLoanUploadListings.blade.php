c24348aa60c914ffcfb30246845b1ea595ff7cd7
lonaxit <lonaxit@outlook.com>
2021-03-15 11:46:48 +0100

    Modeified display

--- resources/views/IppisAnalysis/masterLoanUploadListings.blade.php
+++ resources/views/IppisAnalysis/masterLoanUploadListings.blade.php
@@ -39,14 +39,6 @@
                         <td>{{$myItem->ippis_no}}</td>
                         <td>{{number_format($myItem->cumulative_amount,2,'.',',')}}</td>
                         <td>{{$myItem->created_at->diffForHumans()}}</td>
-                        <td>
-                            @if($myItem->unPostedDeduction($myItem->id)->count()==0)
-                            <a href="/postDeductions/{{$myItem->id}}" class="btn red darken-3 post-looan">Post Anyway</a>
-                            @else
-
-                            @endif
-
-                        </td>
                     </tr>
                     @endforeach
                 </tbody>
