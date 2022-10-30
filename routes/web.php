9523f4ae87787e4dd6ba1626a88979a02d644727
lonaxit <lonaxit@outlook.com>
2022-10-30 10:04:30 +0100

    Added new route

--- routes/web.php
+++ routes/web.php
@@ -328,7 +328,9 @@ Route::middleware(['auth:admin'])->group(function () {
   Route::get('/savingMaster/listing/{date}/{reference}','IppisAnalysisController@recentMasterSaving');
   Route::get('/post/loans','IppisAnalysisController@recentIppisLoanInputs');
   Route::get('/loandeductions/bulkmaster','IppisAnalysisController@postLoanBulk');
-  Route::get('/postDeductions/{id}','IppisAnalysisController@postLoanAnyway');
+
+  Route::get('/postDeduction/{id}','IppisAnalysisController@postSingleMasterDeduction');
+
   Route::get('/saving/distribute/{date}/{ref}','IppisAnalysisController@postSaving');
   Route::get('/saving/post/{id}','IppisAnalysisController@postMySaving');
   Route::get('/saving-master-upload-form','IppisAnalysisController@savingMasterForm');
